4c97c5fea724b0cf4894da8d0e1cdc0584c863f6
viera <64090268+niceddosbro@users.noreply.github.com>
2024-06-20 10:13:54 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,10 @@
-{
-  {
-    user = "wahta1337",
-    blacklisted = false
-  },
-  {
-    user = "sommergewitter",
-    blacklisted = true
-  }
-}
+[  
+  [
+    "user": "wahta1337",
+    "blacklisted":false
+  ],
+  [
+    "user": sommergewitter",
+    "blacklisted": true
+  ]
+]
